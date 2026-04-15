265d6143d4462ab9d150922371e50075fc32c899
Rodrigo Antonio Sánchez Zavala <137818654+TabulateTech@users.noreply.github.com>
2026-04-15 14:53:19 -0600

    Add zoom factor parameter to PointCloudViewer constructor

--- include/viewer.h
+++ include/viewer.h
@@ -7,7 +7,7 @@
 
 class PointCloudViewer {
 public:
-    PointCloudViewer(int width, int height, float max_range_m, std::string output_dir);
+    PointCloudViewer(int width, int height, float max_range_m, float zoom_factor, std::string output_dir);
     ~PointCloudViewer();
 
     bool createWindow(const std::string& title);
@@ -19,10 +19,13 @@ private:
     void saveSvg(const LaserScan& scan) const;
     int toScreenX(float x_m) const;
     int toScreenY(float y_m) const;
+    bool isInsideViewport(int x, int y) const;
+    float visibleRangeM() const;
 
     int width_ = 900;
     int height_ = 900;
     float max_range_m_ = 10.0f;
+    float zoom_factor_ = 1.0f;
     std::string output_dir_;
 
 #ifdef _WIN32
